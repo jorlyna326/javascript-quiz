0044586af8e8de30cf8e131801e5742a1620afb1
Jorlyna Pujols <jorlynapujols@Jorlynas-MBP.attlocal.net>
2023-10-04 01:22:25 -0400

    add JS functions and additional CSS

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# javascript-quiz
+
+# Javascript Quiz
+
+## Mini Javi Quiz
+
+- I was motivated by learning how to use JS
+- I built this project to learn how to use JS , CSS, and HTML all together
+- I had a problem with toggling on my JS but I was able to do some study group sessions which helped me pick up on new things.
+- I learned that little by little, I'm officially learning to code!
+
+## Check it out here!
+jorlyna326.github.io/javascript-quiz
+
+## Screenshot
+LINK SCREENSHOT
--- script.js
+++ script.js
@@ -1,29 +1,11 @@
 // script.js
-
-document.getElementById('Start Quiz').addEventListener('click', function () {
-    toggleSections('section-1', 'section-2');
-});
-
-document.getElementById('View High Scores').addEventListener('click', function () {
-    toggleSections('section-4', 'section-2');
-});
-
-document.getElementById('Go-Back-Button').addEventListener('click', function () {
-    toggleSections('section-2', 'section-4');
-});
-
-document.getElementById('Clear-Scores-Button').addEventListener('click', function () {
-    document.getElementById('scoresCleared').style.display = 'block';
-});
-
-function toggleSections(showId, hideId) {
-    document.getElementById(showId).style.display = 'block';
-    document.getElementById(hideId).style.display = "none";
-}
-h1 = document.queryselector("section-2 h1");
-h1.textcontent = javascriptQuestions[0].question;
-
-const javascriptQuestions = [
+var footerEl = document.getElementById("footer");
+var ulEl = document.getElementById("ul-element");
+var startBtn = document.getElementById("start");
+var h1El = document.querySelector(".h1")
+var currentIndex = 0;
+let time = 60;
+var javascriptQuestions = [
     {
         question: "What does the 'DOM' stand for in JavaScript?",
         correctAnswer: "Document Object Model",
@@ -49,9 +31,76 @@ const javascriptQuestions = [
         correctAnswer: "It iterates over the elements of an array and executes a provided function for each element.",
         allAnswers: ["It sorts the elements of an array.", "It filters the elements of an array based on a condition.", "It iterates over the elements of an array and executes a provided function for each element.", "It reverses the elements of an array."],
     },
-];
 
-If(timer === 0)
-{
+];
+function startquiz() {
+    console.log("i am clicking the start button")
+    toggleSections("section-2", "section-1");
+    time = 60;
+    // start the timer
+    const element = document.getElementById("timer");
+    var setTimer = setInterval(function () {
+        element.innerHTML = "Timer : " + time;
+        time--;
+        if (time <= 0) {
+            element.innerHTML = "Timer : " + 0;
+            clearInterval(setTimer)
+        }
+    }, 1000);
+    displayQuestions();
+}
+function displayQuestions() {
+
+    ulEl.innerHTML = "";
+    var question = javascriptQuestions[currentIndex].question;
+    var answers = javascriptQuestions[currentIndex].allAnswers;
+    h1El.textContent = question;
+    for (let i = 0; i < answers.length; i++) {
+        var liEl = document.createElement("li");
+        liEl.className = "button";
+        liEl.innerHTML = answers[i];
+        liEl.onclick = displayAnswers;
+        ulEl.appendChild(liEl)
+    }
+
+};
+
+function displayAnswers(event) {
+    console.log(event.target.textContent);
+    if (event.target.textContent == javascriptQuestions[currentIndex].correctAnswer) {
+        console.log("correct");
+    } else {
+        console.log("incorrect");
+        time -= 10;
+
+    }
+
+    currentIndex++;
+    if (currentIndex > 4) {
+        toggleSections("section-3", "section-2");
+        document.getElementById("score").textContent = `your final score is ${time}`;
+        
+    } else {
+        displayQuestions();
+    }
+
+}
+
+document.getElementById("Go-Back-Button").addEventListener("click", function () {
+    toggleSections("section-2", "section-4");
+});
+
+document.getElementById("Clear-Scores-Button").addEventListener("click", function () {
+    document.getElementById('scoresCleared').style.display = "block";
+});
+// hide and show page sections
+function toggleSections(showId, hideId) {
+    document.getElementById(showId).style.display = "block";
+    document.getElementById(hideId).style.display = "none";
+}
+
+startBtn.addEventListener('click', startquiz)
+
+if (timer === 0) {
     EndQuiz();
 }
